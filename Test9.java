68c6d0132b1582767d63445c4a86027d9cbce748
AshrafulAhmed536 <59216747+AshrafulAhmed536@users.noreply.github.com>
2019-12-28 03:10:08 -0800

    Update Test9.java

--- Test9.java
+++ Test9.java
@@ -25,11 +25,9 @@ class Student {
 		return rno;
 	}
 
-
 	public void setRno(int rno) {
 		this.rno = rno;
 	}
-
 
 	public String getName() {
 		return name;
@@ -39,15 +37,11 @@ class Student {
 	public void setName(String name) {
 		this.name = name;
 	}
-
-
+	
 	@Override
 	public String toString() {
 		return "Roll NO: " + rno + ", Name: " + name + ", Age: "+age;
-	}
-	
-	
-	
+	}	
 }
 
 class Training {
